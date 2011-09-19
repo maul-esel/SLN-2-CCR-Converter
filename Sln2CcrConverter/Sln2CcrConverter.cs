1f5ba3f2dca3816b2c47ffac1c84a9f7c8eedfce
maul.esel <maul.esel@go4more.de>
2011-09-20 00:30:50 +0200

    some more work on parsing (mostly comments ;-))

--- Sln2CcrConverter/Sln2CcrConverter.cs
+++ Sln2CcrConverter/Sln2CcrConverter.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.IO;
 using System.Text.RegularExpressions;
+using System.Windows.Forms;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using ChameleonCoder.Plugins;
 using System.Xml;
-using System.Windows.Forms;
+using ChameleonCoder.Plugins;
 
 namespace Sln2CcrParser
 {
@@ -63,37 +64,154 @@ namespace Sln2CcrParser
 
         public void Execute()
         {
+            /*
+             * TODO:
+             * first show a window,
+             * letting the user set some options,
+             * opening (multiple) files,
+             * evtl. also supporting reverse-conversion,
+             * specifying the output file(s),
+             * ...
+             * 
+             * It would be best if everything, even the resource types being created,
+             * were customizable.
+             */
+            string file = null;
+
             using (var dialog = new OpenFileDialog() { Filter = "VS solution files (*.sln)|*.sln" })
             {
                 if (dialog.ShowDialog() == DialogResult.OK)
                 {
-                    var text = System.IO.File.ReadAllText(dialog.FileName);
-
-                    if (Regex.IsMatch(text, @"^\s*Microsoft Visual Studio Solution File, Format Version 11.00\s*$", RegexOptions.ExplicitCapture | RegexOptions.Multiline))
-                    {
-                        var projects = ParseProjects(text);
-
-                        var document = new XmlDocument();
-                        document.LoadXml(initialXml);
-
-                        var resources = (XmlElement)document.SelectSingleNode("/cc-resource-file/resources");
-                        foreach (var project in projects)
-                        {
-                            var node = document.CreateElement("project");
-
-                            node.SetAttribute("name", project.Item2);
-                            node.SetAttribute("id", project.Item4.ToString("b"));
-
-                            resources.AppendChild(node);
-
-                            // todo: create resource-data elements & set "created at", "last modified", ...
-
-                            // todo: parse project file
-                        }
-                        document.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.ccr");
-                    }
-                    System.Windows.MessageBox.Show("complete!");
+                    file = dialog.FileName;
                 }
+            }
+
+            if (!string.IsNullOrWhiteSpace(file))
+            {
+                var dir = Path.GetDirectoryName(file);
+                var text = File.ReadAllText(file);
+
+                if (IsValidSLN(text))
+                {
+                    ccrDocument.LoadXml(initialXml);
+                    var projects = ParseProjects(text);                    
+
+                    /* todo: also allow only parsing of a *.csproj file, detect the difference */
+
+                    foreach (var project in projects)
+                    {
+                        var node = ccrDocument.CreateElement("project"); // create the project definition
+                        node.SetAttribute("name", project.Item2);
+                        node.SetAttribute("id", project.Item4.ToString("b"));
+                        ccrDocument.SelectSingleNode("/cc-resource-file/resources").AppendChild(node);
+
+                        var data = ccrDocument.CreateElement("resource-data"); // create the resource-data element
+                        data.SetAttribute("id", project.Item4.ToString("b"));
+                        ccrDocument.SelectSingleNode("/cc-resource-file/data").AppendChild(data);
+
+                        var created = ccrDocument.CreateElement("created"); // create the "created at" date
+                        created.InnerText = DateTime.Now.ToString("yyyyMMddHHmmss");
+                        data.AppendChild(created);
+
+                        string full = Path.Combine(dir, project.Item3);
+                        if (File.Exists(full))
+                        {
+                            var csproj = File.ReadAllText(full);
+                            projDocument.LoadXml(csproj);
+
+                            var xmlNsMan = new XmlNamespaceManager(projDocument.NameTable);
+                            xmlNsMan.AddNamespace("vs", "http://schemas.microsoft.com/developer/msbuild/2003");
+
+                            /*
+                             * NOTE:
+                             * to make this working ideally,
+                             * some language-specific resource or RichContent types would be necessary.
+                             * Plus some decisions. ;-)
+                             * 
+                             * For example, we could store assembly references as
+                             *  - references to resources to be included in the file
+                             *  - RichContent
+                             *  - a resource property
+                             *  - metadata
+                             *  - ...
+                             *  
+                             *  We could store compiler information as
+                             *  - metadata
+                             *  - resource properties
+                             *  - RichContent
+                             *  - ...
+                             *  
+                             * =====================================
+                             * 
+                             * NOTE:
+                             * when parsing resources, we should use the hierarchical structur in VS,
+                             * so we need to create "Group" resources for folders.
+                             * So we have to detect whether a resource is in the top directory or not,
+                             * including support for multiple levels of groups.
+                             * 
+                             * Another thing is the hierarchy e.g. between a *.resx file and the Designer code,
+                             * which should be covered, too.
+                             */
+                            foreach (XmlElement incl in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:Compile", xmlNsMan))
+                            {
+                                /* create code resources */
+                            }
+
+                            foreach (XmlElement res in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:EmbeddedResource", xmlNsMan))
+                            {
+                                /* create file resources */
+                                var fileres = ccrDocument.CreateElement("file");
+                                fileres.SetAttribute("path", res.GetAttribute("Include"));
+                                fileres.SetAttribute("id", Guid.NewGuid().ToString("b"));
+                                fileres.SetAttribute("name", Path.GetFileName(res.GetAttribute("Include")));
+
+                                node.AppendChild(fileres);
+                            }
+
+                            foreach (XmlElement res in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:None", xmlNsMan))
+                            {
+                                /* create file resources */
+                                var fileres = ccrDocument.CreateElement("file");
+                                fileres.SetAttribute("path", res.GetAttribute("Include"));
+                                fileres.SetAttribute("id", Guid.NewGuid().ToString("b"));
+                                fileres.SetAttribute("name", Path.GetFileName(res.GetAttribute("Include")));
+
+                                node.AppendChild(fileres);
+                            }
+
+                            foreach (XmlElement res in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:Reference", xmlNsMan))
+                            {
+                                /* create references to .NET assemblies (?) */
+                            }
+
+                            foreach (XmlElement res in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:Resource", xmlNsMan))
+                            {
+                                /* create file resources */
+                                var fileres = ccrDocument.CreateElement("file");
+                                fileres.SetAttribute("path", res.GetAttribute("Include"));
+                                fileres.SetAttribute("id", Guid.NewGuid().ToString("b"));
+                                fileres.SetAttribute("name", Path.GetFileName(res.GetAttribute("Include")));
+
+                                node.AppendChild(fileres);
+                            }
+
+                            foreach (XmlElement res in projDocument.SelectNodes("/vs:Project/vs:ItemGroup/vs:Page", xmlNsMan))
+                            {
+                                /* create file resources or sth. else */
+                                var fileres = ccrDocument.CreateElement("file");
+                                fileres.SetAttribute("path", res.GetAttribute("Include"));
+                                fileres.SetAttribute("id", Guid.NewGuid().ToString("b"));
+                                fileres.SetAttribute("name", Path.GetFileName(res.GetAttribute("Include")));
+
+                                node.AppendChild(fileres);
+                            }
+                        }
+
+                        // todo: parse project file
+                    }
+                    ccrDocument.Save(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\test.ccr");
+                }
+                System.Windows.MessageBox.Show("complete!");
             }
         }
 
@@ -119,6 +237,20 @@ namespace Sln2CcrParser
                         + @"EndProjectSection[\s\r\n]*)*"
                         + @"EndProject");
 
+        #region validation
+
+        private static bool IsValidSLN(string content)
+        {
+            return slnRegex.IsMatch(content);
+        }
+        static Regex slnRegex = new Regex(@"^\s*Microsoft Visual Studio Solution File, Format Version 11.00\s*$", RegexOptions.Multiline);
+
+        #endregion
+
         static string initialXml = "<cc-resource-file><resources/><data/><settings/><references/></cc-resource-file>";
+
+        private XmlDocument ccrDocument = new XmlDocument();
+
+        private XmlDocument projDocument = new XmlDocument();
     }
 }
